28b3ddb6d7fe8406df11e2172f6d8fcf762b351d
Justin Becker (geekman7473) <geekman7473@gmail.com>
2012-11-20 21:20:05 -0500

    Problem #4 Palindrome checking completed

--- Problem 4.cpp
+++ Problem 4.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-long long int largePrime = 600851475143;
 bool isPalin(string input);
 
 int main(){
@@ -9,15 +8,23 @@ int main(){
 }
 
 bool isPalin(string input){
-	bool Palindrome;
-	if (input.size == 0) {
+	if (input.size <= 0) {
 		return(false);
+	} else if (input.size == 1) {
+        return(true)
 	} else if (input.size % 2 == 0){
 		for (int i = 0; i < (input.size/2) ; i++){
 			if (input[i] != input[input.size - (i + 1)]){
-				palindrome = false;
-				break;
-			{
+                return(false);
+			}
 		}
-	} else
+		return(true);
+	} else {
+        for (int i = 0; i < ((inputs.size - 1) /2); i++){
+            if (input[i] != input[input.size - (i + 1)]){
+                return(false);
+            }
+        }
+        return(true);
+	}
 }
