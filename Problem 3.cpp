b9e49f20ed3cefe2dd25422fe08cfd5e21bb1dd4
Justin Becker (geekman7473) <geekman7473@gmail.com>
2012-11-22 12:49:01 -0500

    Added Debugging Tags

--- Problem 3.cpp
+++ Problem 3.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 bool isPrime(long long num);
 
+//#define DEBUG
+
 int main(){
     long long largePrime = 600851475143LL;
 	long long lastPrimeFactor = 0;
@@ -14,20 +16,26 @@ int main(){
 		if (n % 600000000 == 0){
 			cout << n/600000000 << "/1001" << endl;
 		}
+		#ifdef DEBUG
 		cout << lastPrimeFactor << " " <<largePrime % n << " " << isPrime(n) << endl;
 		system("PAUSE");
+		#endif
 	}
 	cout << lastPrimeFactor;
 	system("PAUSE");
 }
 
 bool isPrime(long long num){
+     #ifdef DEBUG
      cout << "test1";
 	system("PAUSE");
+	#endif
 	long double numFloat = num;
 	for (long long i = 2; i <= sqrt(numFloat); i++){
+        #ifdef DEBUG
 	    cout << i << " " << sqrt(numFloat) << endl;
 		system("PAUSE");
+		#endif
 		if (num % i == 0){
 			return (false);
 		}
--- Problem 5.cpp
+++ Problem 5.cpp
@@ -4,7 +4,7 @@
 #define DEBUG
 using namespace std;
 
-const int range = 20;
+const int range = 10;
 
 bool isPrime(int num);
 
@@ -12,24 +12,15 @@ int main(){
     int previousVal;
 	long product = 1, lastVal;
 	for (int i = 2; i < range; i++){
-        #ifdef DEBUG
-        cout << i << endl;
-        #endif
-		for (int x = 1; x*i < range; x++){
-			if (isPrime(i) && (x+1)*i >= range && x*i != previousVal){
-                #ifdef DEBUG
-  	            cout << "Product: " << i*x << " I val:" << i << " x val:" << x;
-                system("PAUSE");
-                #endif
-                product *= i * x;
-                previousVal = i * x;
-			}
-		}
-		#ifdef DEBUG
-		cout << "overall product: " << product << endl;
-		#endif
+		if (product % i != 0){
+        cout << product % i << endl;
+			product *= (product % i);
+		} 
+        if (i > product) {
+               product *= i;
+               }
 	}
-	cout << product;
+	cout << product << " " << 2 % 1;
 	system("PAUSE");
 }
 
