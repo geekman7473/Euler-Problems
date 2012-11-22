b8d0fecfdd46ed7ab898b2bc9ba327f29e3dbe53
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2012-11-21 20:57:40 -0500

    Problem #5 not working, rewriting from scratch

--- Problem 5.cpp
+++ Problem 5.cpp
@@ -1,19 +1,43 @@
 #include<iostream>
+#include<cmath>
 
+#define DEBUG
 using namespace std;
 
-const int range = 10;
+const int range = 20;
+
+bool isPrime(int num);
 
 int main(){
-	int product = 1, lastVal;
-	for (int i = 0; i < range; i++){
-		for (int i2 = 1; i1 < i; i2++){
-			if (i * i2 < range){
-				lastVal = (i *i2)
+    int previousVal;
+	long product = 1, lastVal;
+	for (int i = 2; i < range; i++){
+        #ifdef DEBUG
+        cout << i << endl;
+        #endif
+		for (int x = 1; x*i < range; x++){
+			if (isPrime(i) && (x+1)*i >= range && x*i != previousVal){
+                #ifdef DEBUG
+  	            cout << "Product: " << i*x << " I val:" << i << " x val:" << x;
+                system("PAUSE");
+                #endif
+                product *= i * x;
+                previousVal = i * x;
 			}
 		}
-		product *= lastVal;
+		#ifdef DEBUG
+		cout << "overall product: " << product << endl;
+		#endif
 	}
 	cout << product;
 	system("PAUSE");
+}
+
+bool isPrime(int num){
+	for (int i = 2; i <= sqrt(num); i++){
+		if (num % i == 0){
+			return (false);
+		}
+	}
+	return (true);
 }
