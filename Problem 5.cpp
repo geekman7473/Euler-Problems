380ddd54378d32541ed94948c49814a4d5778fc5
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2012-11-21 19:36:45 -0500

    Started #5, major modification required

---
+++ Problem 5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+const int range = 10;
+
+int main(){
+	int product = 1, lastVal;
+	for (int i = 0; i < range; i++){
+		for (int i2 = 1; i1 < i; i2++){
+			if (i * i2 < range){
+				lastVal = (i *i2)
+			}
+		}
+		product *= lastVal;
+	}
+	cout << product;
+	system("PAUSE");
+}
