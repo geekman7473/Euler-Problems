3beffa8a4efbfadbd69893f09f68b43e631a62f0
Justin Becker (geekman7473) <geekman7473@gmail.com>
2012-11-22 22:33:14 -0500

    Problem 5 finished with problems

--- Problem 5.cpp
+++ Problem 5.cpp
@@ -4,23 +4,30 @@
 #define DEBUG
 using namespace std;
 
-const int range = 10;
+const int range = 20;
 
 bool isPrime(int num);
+long lcm(int num1, int num2);
+int gcf(int num1, int num2);
 
 int main(){
-    int previousVal;
-	long product = 1, lastVal;
-	for (int i = 2; i < range; i++){
-		if (product % i != 0){
-        cout << product % i << endl;
-			product *= (product % i);
-		} 
-        if (i > product) {
-               product *= i;
-               }
+    #ifdef DEBUG
+    cout << gcf(4,8) << endl;
+    system("PAUSE");
+    cout << gcf(8,4) << endl;
+    system("PAUSE");
+    #endif
+    int overall;
+    cout << "1 and 2" << endl;
+    overall = lcm(1,2);
+	for(int i = 3; i < range; i++){
+	    cout << i << endl;
+	    overall = lcm(overall, i);
+        #ifdef DEBUG
+        cout << "Overall LCM: " << overall << endl;
+        #endif
 	}
-	cout << product << " " << 2 % 1;
+	cout << overall;
 	system("PAUSE");
 }
 
@@ -31,4 +38,36 @@ bool isPrime(int num){
 		}
 	}
 	return (true);
+}
+
+long lcm(int num1, int num2){
+    if (num1 == 0 || num2 == 0){
+    return (0);
+    } else {
+        #ifdef DEBUG
+        cout << "num1: " << num1 << " num2:" << num2 << " GCF: "<< gcf(num1, num2) << " OVERALL Output: " << abs(num1 * num2)/gcf(num1, num2) << endl;
+        system("PAUSE");
+        #endif
+    return (abs(num1 * num2)/gcf(num1, num2));
+    }
+}
+
+int gcf(int num1, int num2){
+    int remain, switch_int;
+    if(num1 < num2){
+        switch_int = num1;
+        num1 = num2;
+        num2 = switch_int;
+    }
+    #ifdef DEBUG
+    cout << "larger: " << num1 << " smaller: " << num2;
+    #endif
+    do{
+         remain = num1 % num2;
+         num1 = num2;
+         if (remain != 0){
+         num2 = remain;
+         }
+    }while (remain != 0);
+    return (num2);
 }
