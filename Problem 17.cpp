6bbd28fac88681f537a3eab2e19a14a661f778ab
Justin Becker <geekman7473@gmail.com>
2012-12-03 09:06:56 -0500

    Reworked 17, still not working

--- Problem 17.cpp
+++ Problem 17.cpp
@@ -1,49 +1,23 @@
 #include<iostream>
 #include<cmath>
+#include <cstdlib>
 
-#define DEBUG
+//#define DEBUG
 
 using namespace std;
 
-short hundredsConvert(short num);
-long numOfLetters(int num);
 int digitConversion(int shortNum, int digitsPlace);
-int teenConvert (int num);
+long numOfLetters(int num);
 
 long long int sum = 0;
 
 int main(){
 #ifdef DEBUG
-cout << numOfLetters(314) << " 314 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(350) << " 350 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(999) << " 999 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(125) << " 125 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(90) << " 90 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(8) << " 8 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(11) << " 11 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(12) << " 12 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(13) << " 13 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(14) << " 14 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(15) << " 15 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(16) << " 16 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(17) << " 17 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(18) << " 18 as characters" << endl;
-system("PAUSE");
-cout << numOfLetters(19) << " 19 as characters" << endl;
-system("PAUSE");
+int input = -1;
+while(input != 0){
+            cin >> input;
+            cout << numOfLetters(input);
+}
 #endif
     const int range =  1000;
 
@@ -56,6 +30,9 @@ system("PAUSE");
 }
 
 long numOfLetters(int num){
+    int charsInDigits[11] = {0, 3, 3, 5, 4, 4, 3, 5, 5, 4, 3};
+    int charsInTens[11] = {0, 3, 6, 6, 5, 5, 5, 7, 6, 6};
+    int charsInTeens [11] = {3, 6, 6, 8, 8, 7, 7, 9, 8, 8};
     int tempNum = num;
     int tempArray[4];
     long sum = 0;
@@ -64,7 +41,9 @@ long numOfLetters(int num){
     }
     for (int x = 1; tempNum != 0; x++){ //converts numbers to digits in Temp Array
         tempArray[x - 1] = tempNum % 10;
+        #ifdef DEBUG
         cout << tempNum % 10 << " " << tempNum << endl;
+        #endif
         tempNum -= tempNum % 10;
         tempNum /= 10;
     }
@@ -74,128 +53,31 @@ long numOfLetters(int num){
     }
     system("PAUSE");
     #endif
-    for (int y = 0; y < 3; y++){
-            if (tempArray[1] == 1 && tempArray [0] > 0){
-                sum += teenConvert(tempArray[0]);
-            }else if(tempArray[2] > 0 && tempArray[1] == 0 && tempArray[0] == 0){
-                sum += hundredsConvert(tempArray[2]);
-            } else {
-                sum += digitConversion(tempArray[y], y + 1);
-            }
+    //cout << endl << "temp2: " << tempArray[2] << endl;
+    if (tempArray[2] > 0){
+        sum += charsInDigits[tempArray[2]] + 7;
+        if ((tempArray[1] > 0 || tempArray[0] > 0) && tempArray[2] > 0){
+			cout << "wut";
+            sum += 3;
         }
-    return(sum);
-}
-
-int digitConversion(int shortNum, int digitsPlace){
-        switch (digitsPlace){
-        case 1:
-            switch (shortNum){
-                case 0:
-                    return (0);
-                    break;
-                case 1:
-                case 2:
-                case 6:
-                    return (3);
-                    break;
-                case 4:
-                case 5:
-                case 9:
-                    return (4);
-                    break;
-                default:
-                    return (5);
-                    break;
-        }
-        case 2:
-            switch (shortNum){
-                case 0:
-                    return (0);
-                    break;
-                case 1:
-                    return (3);
-                    break;
-                case 2:
-                case 3:
-                case 8:
-                case 9:
-                    return (6);
-                    break;
-                case 4:
-                case 5:
-                case 6:
-                    return (5);
-                    break;
-                default:
-                    return(7);
-                    break;
-            }
-        case 3:
-            switch (shortNum){
-                case 0:
-                    return (0);
-                    break;
-                case 1:
-                case 2:
-                case 6:
-                    return (13);
-                    break;
-                case 4:
-                case 5:
-                case 9:
-                    return (14);
-                    break;
-                default:
-                    return (15);
-                    break;
-                    }
-        }
-}
-
-int teenConvert (int num){
-
-    switch (num){
-        case 0:
-            return (0);
-            break;
-        case 1:
-        case 2:
-            return (6);
-            break;
-        case 5:
-        case 6:
-            return (7);
-            break;
-        case 3:
-        case 4:
-        case 8:
-        case 9:
-            return (8);
-            break;
-        default:
-            return (9);
-            break;
     }
-
-}
-
-short hundredsConvert(short num){
-            switch (num){
-                case 0:
-                    return (0);
-                    break;
-                case 1:
-                case 2:
-                case 6:
-                    return (10);
-                    break;
-                case 4:
-                case 5:
-                case 9:
-                    return (11);
-                    break;
-                default:
-                    return (12);
-                    break;
-                    }
+    if (tempArray[1] > 1){
+        sum += charsInTens[tempArray[1]];
+	}
+	if (tempArray[0] > 0 && !(num >= 10 && num <= 20)){
+            sum += charsInDigits[tempArray[0]];
+    }
+    //todo: rewrite handleing of teens
+    if (num >= 10 && num <= 20){
+        sum += charsInTeens[tempArray[0]];
+    }
+    /*cout<<"here it goes  ";
+    cout<<tempArray[2]<<"  " <<tempArray[1]<<"   " <<tempArray[0];
+    cout<<num;
+    cout<<"  This is over "; */
+    /*if (num < 10 || (tempArray[2] > 0 && tempArray[0] > 0 && tempArray[1] == 0)){
+			cout << "lol";
+            sum -= 3;
+            }*/
+    return(sum);
 }
