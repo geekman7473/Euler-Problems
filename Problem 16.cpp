b746c1a9e2023ab79048538f81ba4b377c56761c
Justin Becker <geekman7473@gmail.com>
2012-12-19 13:35:36 -0500

    Adding vector to arbitrary ints, also started #19

--- Problem 16.cpp
+++ Problem 16.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
 #include<cmath>
+#include<cstdlib>
+#include<vector>
 
 using namespace std;
 
 void multiply(int base);
 
-#define ARRAYSIZE 2048
+#define ARRAYSIZE 8000
 
-short product [ARRAYSIZE];
-const int productLength = ARRAYSIZE;
+vector<int> product;
+//const int productLength = ARRAYSIZE;
 
 int main(){
 	long exponet = 1000;
     long base = 2;
+while (true){
 	cout << "Enter a positive Base as an integer: ";
 	cin >> base;
 	cout << "Enter a Positive Exponent an integer: ";
@@ -20,30 +23,31 @@ int main(){
 	product[0] = 1;
 	for(int i = 1; i <= exponet; i++){
 		multiply(base);
-		for (int pos = 0; pos < productLength; pos++){//goes through digits
+		for (int pos = 0; pos < product.size() -1; pos++){//goes through digits
 			for (double y = 0; product[pos] >= pow(10, y); y++){//Carrys digits accordingly
 		    	if (y != 0){
               		int YInt = y;
 		       		product[pos + YInt] += floor(product [pos] / pow(10, y));
 		       		product[pos] -= floor(product [pos] / pow(10, y)) * 10;
-           }
+           		}
+			}
 		}
 	}
-}
-	for (int x = productLength - 1; x >= 0 ; x--){
+	for (int x = product.size() - 1; x >= 0 ; x--){
 		cout << product[x] << " ";
 	}
 	cout << endl << endl;
 	long long tempSum = 0;
-	for (int x = 0; x < ARRAYSIZE; x++){
+	for (int x = 0; x < product.size(); x++){
         tempSum += product[x];
 	}
 	cout << tempSum << endl;
-	//system("PAUSE");
+	system("PAUSE");
+}
 }
 
 void multiply(int base){
-	for (int x = productLength - 1; x >= 0 ; x--){
+	for (int x = product.size() - 1; x >= 0 ; x--){
 		product[x] *= base;
 	}
 }
---
+++ Problem 19.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	short dayWeek = 2;
+	long day = 0;
+	short month = 1;
+	short year = 0;
+	while(year <= 998){
+		if (month > 12){
+			month = 0;
+			year++;
+		}
+		
+	}
+}
