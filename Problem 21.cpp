dd3ae100630bb2c81f1686cfc72407aaa7d9d6e0
unknown <Justin Becker@ISAAC_1_R4.(none)>
2013-01-07 22:15:10 -0500

    Finished 21

--- Problem 21.cpp
+++ Problem 21.cpp
@@ -7,7 +7,17 @@ using namespace std;
 long sumDivs (long num);
 
 int main(){
-	cout << sumDivs(220);
+	//cout << sumDivs(220);
+	//cout << sumDivs(220) << " " << sumDivs(sumDivs(sumDivs(220))) << " ";
+	long long sum = 0;
+	for (long i = 1; i < 10000; i++){
+		if (i == sumDivs(sumDivs(i)) && i != sumDivs(i)){
+			cout << i << " " << sumDivs(i) << " " << sumDivs(sumDivs(i)) << endl;
+			//system("PAUSE");
+			sum += i;
+		}
+	}
+	cout << sum;
 	system("PAUSE");
 }
 
