88e53beea84ecfbcb87d7e57fc9e0450aed53231
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2013-08-21 14:12:32 -0400

    Problem #3 reworked to be massively more efficient

--- Problem 3.cpp
+++ Problem 3.cpp
@@ -10,32 +10,18 @@ int main(){
     long long largePrime = 600851475143LL;
 	long long lastPrimeFactor = 0;
 	for (long long n = 2; n <= largePrime - 1; n++){
-		if (largePrime % n == 0 && isPrime(n)){
-			lastPrimeFactor = n;
+		if (largePrime % n == 0 && isPrime(largePrime / n)){
+			lastPrimeFactor = largePrime / n;
+			break;
 		}
-		if (n % 600000000 == 0){
-			cout << n/600000000 << "/1001" << endl;
-		}
-		#ifdef DEBUG
-		cout << lastPrimeFactor << " " <<largePrime % n << " " << isPrime(n) << endl;
-		system("PAUSE");
-		#endif
 	}
 	cout << lastPrimeFactor;
-	system("PAUSE");
+	cin.get();
 }
 
 bool isPrime(long long num){
-     #ifdef DEBUG
-     cout << "test1";
-	system("PAUSE");
-	#endif
 	long double numFloat = num;
 	for (long long i = 2; i <= sqrt(numFloat); i++){
-        #ifdef DEBUG
-	    cout << i << " " << sqrt(numFloat) << endl;
-		system("PAUSE");
-		#endif
 		if (num % i == 0){
 			return (false);
 		}
